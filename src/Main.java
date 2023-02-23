0bb57e38f953b9ddf010aed08e84a2abec3b51fe
slimm1 <tini.ramonda@gmail.com>
2023-02-23 12:15:24 +0100

    Creado constructor y metodo cajablanca para la clase main

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    String name;
+    int year, age;
+    public static void main(String[]Args){
+            System.out.println(metodoCajaBlanca(-1,-3));
+
+    }
+
+    public Main(String name, int age, int year){
+        this.name=name;
+        this.age=age;
+        this.year=year;
+    }
+
+    public static double metodoCajaBlanca(int a, int b){
+
+        double op = 0;
+
+        if( (a>b) && (a>0) ){
+
+            op = b/new Double(a);
+
+            System.out.println("El cociente es " + op);
+        }
+        else if(a>5){
+
+            op=b-a;
+
+            System.out.println("La diferencia es " + op);
+        }
+        else if(b>=-2){
+
+            System.out.println("b>=-2");
+
+            op = a;
+        }
+        return op;
+    }
+
+}
